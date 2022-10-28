da2c15f6b5e558a6905e61410e0e6205710c78a7
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-10-28 19:28:07 -0300

    28 oct

---
+++ GUIA 17 - SPRING 1/bibli/src/main/java/com/mza/bibli/BibliApplication.java
@@ -0,0 +1,14 @@
+package com.mza.bibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliApplication.class, args);
+
+    }//final main
+
+}//final
