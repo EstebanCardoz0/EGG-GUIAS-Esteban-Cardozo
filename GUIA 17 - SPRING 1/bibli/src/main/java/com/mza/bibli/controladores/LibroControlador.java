ed099b57683f982d44e20af822b8dc8ed39f1ca3
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-10-31 12:40:07 -0300

    31 oct

---
+++ GUIA 17 - SPRING 1/bibli/src/main/java/com/mza/bibli/controladores/LibroControlador.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mza.bibli.controladores;
+
+import com.mza.bibli.excepciones.MiException;
+import com.mza.bibli.servicios.AutorServicio;
+import com.mza.bibli.servicios.EditorialServicio;
+import com.mza.bibli.servicios.LibroServicio;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Usuario
+ */
+
+@Controller
+@RequestMapping("/libro")
+public class LibroControlador {
+    
+        @Autowired
+    private LibroServicio libroServicio;
+        @Autowired
+    private AutorServicio autorlServicio;
+        @Autowired
+    private EditorialServicio editorialServicio;
+    
+             @GetMapping ("/registrar") //localhost:8080/libro/registrar
+    public String registrar() {
+        return "libro_form.html";   
+
+    }
+         @PostMapping("/registro")
+    public String registro (@RequestParam (required = false) Long isbn, @RequestParam  String titulo,@RequestParam (required = false) Integer ejemplares,
+            @RequestParam String idAutor, @RequestParam String idEditorial, ModelMap modelo){
+        try {
+            //System.out.println("nombre: "+nombre);
+            
+            libroServicio.crearLibro(isbn,titulo,ejemplares,idAutor,idEditorial);
+        } catch (MiException ex) {
+            Logger.getLogger(EditorialControlador.class.getName()).log(Level.SEVERE, null, ex);
+                    return "libro_form.html";
+
+        }
+        
+        return "index.html";
+    }
+        
+        
+}//final
