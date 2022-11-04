1ceb469d5837eee6ece1238914042e37a9b95181
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-11-03 21:52:25 -0300

    03 nov

--- GUIA 17 - SPRING 1/Noticias/src/main/java/com/Noticias/controladores/NoticiaControlador.java
+++ GUIA 17 - SPRING 1/Noticias/src/main/java/com/Noticias/controladores/NoticiaControlador.java
@@ -6,15 +6,38 @@
 package com.Noticias.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
  * @author Usuario
  */
 @Controller
+@RequestMapping("/noticia")
 public class NoticiaControlador {
-        
+
 //    Esta clase tiene la responsabilidad de llevar adelante las funcionalidades necesarias para operar
 //con la vista del usuario diseñada para la gestión de noticias (guardar/modificar noticia, listar
 //noticias, dar de baja, etc).
-}
+    @GetMapping("/guardar") //localhost:8080/autor/guardar
+    public String guardar() {
+        return "noticia_form.html";
+
+    }
+    
+    @PostMapping("/guardado")
+    public String guardado(@RequestParam String titulo,@RequestParam String cuerpo){
+    
+       System.out.println("TITULO ====== "+titulo);
+        System.out.println("CUERPO ====== "+cuerpo);
+        
+        
+        
+    return "noticia_form.html";     
+    }
+    
+
+}//FINAL
