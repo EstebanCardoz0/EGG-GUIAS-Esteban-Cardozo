9612cbc442f56ce9fcd2811ffb35a634165c78c9
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-11-24 12:50:54 -0300

    24 nov

--- GUIA 17 - SPRING 1/CursoTH/src/main/java/Curso/ControladorREST.java
+++ GUIA 17 - SPRING 1/CursoTH/src/main/java/Curso/ControladorREST.java
@@ -6,11 +6,15 @@
 package Curso;
 
 import Curso.domain.Individuo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -19,32 +23,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorREST {
-    
-    @Value("${indice.hola2}")
-    private String hola2;
-    
+
+//    @Value("${indice.hola2}")
+//    private String hola2;
+
     @GetMapping("/")
-    
     public String primerComienzo(Model modelo) {
-        
+
         String hola = "hola mundo con spring boot";
-        
-        Individuo indi = new Individuo();
-        
-        indi.setNombre("Antonio");
-        indi.setApellido("Valencia");
-        indi.setCorreo("avalencia@bdc.com");
-        log.info("Estoy ejecutando el controlador spring mvc");
-        indi.setTelefono("8877665544");
-        indi.setEdad("35 años");
-        
-        modelo.addAttribute("hol", hola);
-        modelo.addAttribute("hola1", hola2);
-        modelo.addAttribute("individuo", indi);
-        
-        
-        return "indice";
-        
+
+        Individuo individuo = new Individuo();
+
+        individuo.setNombre("Antonio");
+        individuo.setApellido("Valencia");
+        individuo.setCorreo("avalencia@bdc.com");
+        individuo.setTelefono("8877665544");
+        individuo.setEdad("35 años");
+
+        Individuo individuo2 = new Individuo();
+
+        individuo2.setNombre("walter");
+        individuo2.setApellido("cardozo");
+        individuo2.setCorreo("walter@cardozo");
+        individuo2.setTelefono("8877665544");
+        individuo2.setEdad("58 años");
+
+//        List<individuo> individuos= new ArrayList();
+//        individuos.add(indi);
+//        individuos.add(indi2);
+        List individuos = Arrays.asList(individuo, individuo2);
+
+//        log.info("Estoy ejecutando el controlador spring mvc");
+//        modelo.addAttribute("hola", hola);
+//        modelo.addAttribute("hola2", hola2);
+        modelo.addAttribute("individuo", individuo);
+        modelo.addAttribute("individuos", individuos);
+
+        return "indice.html";
+
     }
-    
+
 }
