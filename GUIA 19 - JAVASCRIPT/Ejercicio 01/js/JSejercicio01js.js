5621683e804dc9e6492586425b9bf88d93583a62
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-12-12 21:56:20 -0300

    12 diciembre

---
+++ GUIA 19 - JAVASCRIPT/Ejercicio 01/js/JSejercicio01js.js
@@ -0,0 +1,11 @@
+var saludo= window.prompt("Cómo está el día de hoy? (soleado, nuboso, lloviendom etc.)")
+
+window.alert("El dia de hoy está "+saludo)
+
+// switch(saludo){
+
+// case 'soleado': window.alert("el dia está soleado");break;
+
+// default: window.alert("msje nulo");
+
+// }
