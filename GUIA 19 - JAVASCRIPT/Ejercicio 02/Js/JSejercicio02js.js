1025da2dc4f72ba4c08d19afc367468aaa673d40
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-12-14 12:40:20 -0300

    14 dic

---
+++ GUIA 19 - JAVASCRIPT/Ejercicio 02/Js/JSejercicio02js.js
@@ -0,0 +1,7 @@
+var circun= prompt("Ingrese el valor del radio de la circunferencia");
+
+
+var area= 3.14159*(Math.pow(circun,2));
+var perimetro= 2*3.14159*circun;
+
+window.alert("Área: "+ area +"\nPerímetro: "+ perimetro);
---
+++ GUIA 19 - JAVASCRIPT/Ejercicio 03/Js/JSejercicio03js.js
@@ -0,0 +1,13 @@
+//  var str= "Mi texto"; 
+// document.getElementById("texto").innerHTML=str; 
+
+var edad;
+
+// function sub() {
+
+//     edad = document.getElementById("prod").value;
+//     //alert("Tu edad es: " + edad);
+
+//     let resp= "Tenés "+ edad+" años";
+
+// };
