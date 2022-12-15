4ca89c16083c47fb10b2b58d0a67b06b1cf0ac66
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-12-14 21:54:30 -0300

    14 diciembre

--- GUIA 19 - JAVASCRIPT/Ejercicio 03/Js/JSejercicio03js.js
+++ GUIA 19 - JAVASCRIPT/Ejercicio 03/Js/JSejercicio03js.js
@@ -11,3 +11,14 @@ var edad;
 //     let resp= "Tenés "+ edad+" años";
 
 // };
+
+edad = window.prompt("¿Qué edad tenés?");
+
+if (edad < 18) {
+    window.alert("Sos menor de edad");
+
+} else {
+    window.alert("Sos mayor de edad");
+
+}
+
