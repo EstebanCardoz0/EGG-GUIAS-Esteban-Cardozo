af0531979f10ddedeb703f8183ca208bde4d84c9
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-12-19 22:00:52 -0300

    19 diciembre

--- GUIA 19 - JAVASCRIPT/Ejercicio 05/Js/JSejercicio05js.js
+++ GUIA 19 - JAVASCRIPT/Ejercicio 05/Js/JSejercicio05js.js
@@ -4,12 +4,34 @@
 // carácter de la operación que desea realizar: ‘S' o ‘s’ para la suma, ‘R’ o ‘r’ para la resta, ‘M’
 // o ‘m’ para la multiplicación y ‘D’ o ‘d’ para la división.
 
+var num1= prompt("elegi un numero",0);
+var aux1= parseInt(num1) ;
+
+var num2= prompt("elegi otro numero",0);
+var aux2= parseInt(num2) ;
+
+var opc= prompt("Qué operacion querés realizar (s,r,m,d)")
+
+switch (opc){
+
+case "s": alert(aux1+aux2); break;
+case "r": alert(aux1-aux2); break;
+case "m": alert(aux1*aux2); break;
+case "d": alert(aux1/aux2); break;
+default: alert("caracter invalido");
 
 
-var num1= prompt("Ingresá el primer número");
-var num2= prompt("Ingresá el segundo número");
+}
 
-alert(num1+num2);
+// var suma= aux1+aux2;
+
+// alert(suma);
+
+
+
+// var num2= prompt("Ingresá el segundo número");
+
+// alert(num1+num2);
 
 
 // if (caracter=="s" ||caracter=="n"||caracter=="N"||caracter=="S") {
---
+++ GUIA 19 - JAVASCRIPT/Ejercicio 06/Js/JSejercicio06js.js
@@ -0,0 +1,37 @@
+// Realizar un programa que, dado un número entero, visualice en pantalla si es par o impar.
+// En caso de que el valor ingresado sea 0, se debe mostrar “el número no es par ni impar”.
+
+var num1= prompt("ingresá un numero");
+var aux1= parseInt(num1) ;
+
+if(aux1==0){
+alert("El número no es par ni impar");
+
+}else if(aux1%2==0){
+    alert("El número es par");
+}else{
+alert("El número es impar");
+}
+
+
+
+
+
+// var suma= aux1+aux2;
+
+// alert(suma);
+
+
+
+// var num2= prompt("Ingresá el segundo número");
+
+// alert(num1+num2);
+
+
+// if (caracter=="s" ||caracter=="n"||caracter=="N"||caracter=="S") {
+//     alert("CORRECTO")
+// } else {
+//     alert("INCORRECTO")
+    
+// }
+
