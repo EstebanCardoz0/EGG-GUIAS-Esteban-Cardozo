6445b9986fb549a0f06314948007fab9f8986db7
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2022-12-29 12:43:38 -0300

    29 dic

--- GUIA 20 - REACT/React prueba/mascotapp/src/components/public/footer.js
+++ GUIA 20 - REACT/React prueba/mascotapp/src/components/public/footer.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
-    return (
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
+  return (
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
 
-
-
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-                </ul>
-            </footer>
-        </div>
-    )
-}
-
-
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
